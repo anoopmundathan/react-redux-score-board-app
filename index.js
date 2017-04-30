9ad52094694ee70f7d8034752fb1e245b9af05ae
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-30 05:40:37 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ScoreBoard from './ScoreBoard';
+
+ReactDOM.render(
+  <ScoreBoard />,
+  document.getElementById('root')
+);
+
