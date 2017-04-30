e07d6e9b889cf3b841f53a7c50d9a7d018d9acc2
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-30 07:15:55 +0100

    Seperate Player component

--- src/containers/ScoreBoard.js
+++ src/containers/ScoreBoard.js
@@ -5,6 +5,7 @@ import Stopwatch from '../components/Stopwatch';
 import Stats from '../components/Stats';
 import Counter from '../components/Counter';
 import AddPlayerForm from '../components/AddPlayerForm';
+import Player from '../components/Player';
 
 const INITIAL_STATE = {
   players: [
@@ -88,26 +89,6 @@ Header.propTypes = {
 
 // ----------------------------------------------------------------------
 
-function Player(props) {
-  return (
-    <div className="player">
-      <div className="player-name">
-        <a className="remove-player" onClick={props.onRemove}>✖</a>
-        {props.name}
-      </div>
-      <div className="player-score">
-        <Counter onChange={props.onScoreChange} score={props.score} />
-      </div>
-    </div>
-  );
-}
-
-Player.propTypes = {
-  name: React.PropTypes.string.isRequired,
-  score: React.PropTypes.number.isRequired,
-  onRemove: React.PropTypes.func.isRequired,
-  onScoreChange: React.PropTypes.func.isRequired,
-};
 
 // ----------------------------------------------------------
 
