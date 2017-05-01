fcdf400bd5d93b3c349ae8de7d75a384cea5c21f
anoopmundathan <anoop.mundathan@gmail.com>
2017-05-01 07:22:23 +0100

    Create store

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player';
+
+// Create store
+const store = createStore(
+  PlayerReducer
+);
+
 import ScoreBoard from './src/containers/ScoreBoard';
 
 ReactDOM.render(
-  <ScoreBoard />,
+  <Provider store={ store }>
+    <ScoreBoard />
+  </Provider>,
   document.getElementById('root')
 );
 
