f2f43d58fcff443d11d790d71c769bd3b6ed5b2d
anoopmundathan <anoop.mundathan@gmail.com>
2017-05-02 06:54:26 +0100

    Add devtoolsextension for redux

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import PlayerReducer from './src/reducers/player';
+import ScoreBoard from './src/containers/ScoreBoard';
 
 // Create store
 const store = createStore(
-  PlayerReducer
+  PlayerReducer,
+  window.devToolsExtension && window.devToolsExtension()
 );
-
-import ScoreBoard from './src/containers/ScoreBoard';
 
 ReactDOM.render(
   <Provider store={ store }>
